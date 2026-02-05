0321e628a28801fb5ebdf63fcca6967e9f60fb5f
naraanjo <185625172+naraanjo@users.noreply.github.com>
2026-02-05 16:26:07 +0100

    Gitignore

--- GestorHerores/obj/Debug/net9.0/GestorHerores.AssemblyInfo.cs
+++ GestorHerores/obj/Debug/net9.0/GestorHerores.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorHerores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+62fad5454a6038d8e3ca2eb31d72078f8c134ec2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d29788588d6cff9706711f50c2d3d229b0c10114")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorHerores")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorHerores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
